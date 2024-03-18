6aed0e7b4d889b0c0723fd89a1d8a4e1f6d9d8f2
Umer <u.farooq@4dmagic.pk>
2024-03-18 16:54:17 +0500

    initial

--- obj/Debug/net7.0/backend.AssemblyInfo.cs
+++ obj/Debug/net7.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a77038ee8fe9e6a48c2235c653214c63ebc471f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138facec186d8473c83c96397d6225492f61bb89")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
