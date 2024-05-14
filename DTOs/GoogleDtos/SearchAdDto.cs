f06d65c8747c43af907624d732951fdb5afc47f2
Fatima <k200406@nu.edu.pk>
2024-05-14 19:24:58 +0500

    Create google search ad

--- DTOs/GoogleDtos/SearchAdDto.cs
+++ DTOs/GoogleDtos/SearchAdDto.cs
@@ -2,8 +2,10 @@
 {
     public class SearchAdDto
     {
-        public string CustomizerAttributeName { get; set; }
-        public string CustomizerAttributePrice { get; set; }
+        public List<string> Headlines { get; set; }
+        public List<string> Descriptions { get; set; }
         public string TargetUrl { get; set; }
+        public string? Path1 { get; set; }
+        public string? Path2 { get; set; }
     }
 }
